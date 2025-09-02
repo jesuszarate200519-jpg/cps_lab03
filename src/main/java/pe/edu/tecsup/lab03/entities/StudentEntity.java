69035070e9659a5abfa54757407c9c5bcf62cd6f
Edson Pinto <ecpm3105@gmail.com>
2025-09-01 20:49:08 -0500

    Second Commit - Edson

---
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -0,0 +1,5 @@
+package pe.edu.tecsup.lab03.entities;
+
+public class StudentEntity {
+    //TO DO
+}
---
+++ src/main/java/pe/edu/tecsup/lab03/servicies/StudentService.java
@@ -0,0 +1,5 @@
+package pe.edu.tecsup.lab03.servicies;
+
+public class StudentService {
+    //TO DO
+}
